97fb44ddf69ffa96317075a40085b0fd94577061
tsprunck <tom.sprunck@gmail.com>
2023-02-13 18:14:32 +0100

    added non constant permittivity management to scheme

--- linalg.cpp
+++ linalg.cpp
@@ -11,6 +11,27 @@ Array2 Array2::solve(Array2 bc, Array2 permittivity, double tol, int maxiter, do
     double error = tol + 1.;
     int iter = 0;
     double currval;
+    // precompute the scheme coefficients depending on permittivity
+    Array2 up_coeff(ny, nx);
+    Array2 down_coeff(ny, nx);
+    Array2 left_coeff(ny, nx);
+    Array2 right_coeff(ny, nx);
+
+    for (int i=1; i < ny-1; i++)
+    {
+        for (int j=1; j < nx-1; j++)
+        {
+            float center_coeff = 1./(permittivity(i,j) + permittivity(i-1,j) + 
+                                    permittivity(i,j-1) + permittivity(i-1,j-1));
+            right_coeff(i,j) = 0.5*(permittivity(i,j) + permittivity(i-1,j))*center_coeff;
+            up_coeff(i,j) = 0.5*(permittivity(i,j) + permittivity(i,j-1))*center_coeff;
+            left_coeff(i,j) = 0.5*(permittivity(i,j-1) + permittivity(i-1,j-1))*center_coeff;
+            down_coeff(i,j) = 0.5*(permittivity(i-1,j) + permittivity(i-1,j-1))*center_coeff;
+        }
+    }
+
+
+
     while (error > tol && iter < maxiter)
     {
         iter++;
@@ -25,7 +46,9 @@ Array2 Array2::solve(Array2 bc, Array2 permittivity, double tol, int maxiter, do
                 if (bc(i,j) == 0)
                 {
                     currval = (*this)(i,j);
-                    (*this)(i,j) = (1-omega)*currval + omega*0.25*((*this)(i+1,j) + (*this)(i-1,j) + (*this)(i,j+1) + (*this)(i,j-1));
+                    (*this)(i,j) = ((1-omega)*currval + 
+                                     omega*((*this)(i+1,j)*up_coeff(i,j) + (*this)(i-1,j)*down_coeff(i,j) + 
+                                     (*this)(i,j+1)*right_coeff(i,j) + (*this)(i,j-1)*left_coeff(i,j)));
                     
                     error += std::abs(currval - (*this)(i,j));
                 }
--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "linalg.hpp"
 
 int main() {
-    int ny =500;
-    int nx = 750;
+    int ny =400;
+    int nx = 500;
     std::cout << "Nb cells: " << ny*nx << std::endl;
 
     // create a 2D array of size nyXnx
@@ -14,10 +14,22 @@ int main() {
     Array2 bc(ny, nx);
     bc.fill(0.0);
 
+    // create a permittivity array
+    Array2 permittivity(ny, nx);
+    permittivity.fill(1.0);
+    // set a centered square to a different value
+    int length = 100;
+    for (int i = ny/2-length; i < ny/2+length; i++) {
+        for (int j = nx/2-length; j < nx/2+length; j++) {
+            permittivity(i, j) = 10.0;
+        }
+    }
+
+
     // fill every border with 1.0
     for (int i = 0; i < ny; i++) { // i is the y coordinate
-        a(i, 0) = 1.0;
-        a(i, nx-1) = 1.0;
+        a(i, 0) = -1.0;
+        a(i, nx-1) = -1.0;
         bc(i, 0) = 1.0;
         bc(i, nx-1) = 1.0;
     }
@@ -29,8 +41,6 @@ int main() {
         bc(ny-1, j) = 1.0;
     }
 
-    a.solve(bc, bc, 1e-4, 100000, 1.9, true);
+    a.solve(bc, permittivity, 1e-5, 100000, 1.9, true);
     a.to_file("output.txt");
-
-    
 }
